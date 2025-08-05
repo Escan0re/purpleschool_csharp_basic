83b66df7d87c989f36c3dce83599fb1a336df6cb
Sergey Rogov <s.a.rogov20@gmail.com>
2025-08-05 14:07:20 +0300

    HomeLibrary init

---
+++ HomeLibrary/Program.cs
@@ -0,0 +1,6 @@
+﻿var bookName = Console.ReadLine();
+var authorName = Console.ReadLine();
+var year =  Console.ReadLine();
+var ISBN = Console.ReadLine();
+
+Console.WriteLine($"{bookName}, {authorName}, {year}, {ISBN}");
